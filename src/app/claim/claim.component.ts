3d1cb00cddd3da7c3bc322f22992c096ce5760de
Guv <Guv@thehouseshop.com>
2020-06-01 21:17:43 +0100

    finsihed claim component

--- src/app/claim/claim.component.ts
+++ src/app/claim/claim.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export class ClaimClass {
+  amount: number;
+  prize: string;
+  game: string;
+  countdown: number;
+}
+
 @Component({
   selector: 'app-claim',
   templateUrl: './claim.component.html',
   styleUrls: ['./claim.component.scss']
 })
 export class ClaimComponent implements OnInit {
+
+  claim: ClaimClass = {
+    amount: 50,
+    prize: 'Free Spins',
+    game: 'Gemix',
+    countdown: 25
+  };
 
   constructor() { }
 
